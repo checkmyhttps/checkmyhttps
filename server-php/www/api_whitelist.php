b0d0cb9f098735d861685b5bd1edaaa60b55643f
Tom H <Tom.HOUDAYER@orange.fr>
2018-05-06 22:15:52 +0100

    Add Twitter, Google and YouTube to whitelisted domains

--- server-php/www/api_whitelist.php
+++ server-php/www/api_whitelist.php
@@ -1,9 +1,15 @@
 <?php
 
 return [
-	'date' => '20171210',
+	'date' => '20180506',
 
 	'domains' => [
+		'twitter.com',
+		'www.youtube.com',
 		'search.employment.gov.bc.ca'
+	],
+
+	'domains_re' => [
+		'(?:google|blogspot)\.[a-z]+(?:\.[a-z]+)?$'
 	]
 ];
