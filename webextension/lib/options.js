b26c268a94648b3e1bf708e17142f37da3d4db48
CheckMyHTTPS <info@checkmyhttps.net>
2022-01-06 00:51:18 +0100

    Update options.js

--- webextension/lib/options.js
+++ webextension/lib/options.js
@@ -39,7 +39,7 @@ CMH.options.settings = {
   alertOnUnicodeIDNDomainNames:  true,
   disableNotifications:          false, 
   checkServerUrl:                'https://checkmyhttps.net/',
-  checkServerFingerprintsSha256: '8B944DBC442DA0892C307DEF6C971B671267BF033E8479C567E173973F970B9F'
+  checkServerFingerprintsSha256: '6D9BBB554CE7CD4420C26F60DD0831D40C34BB07F93E874CF631C24AB9F08F57'
 }
 
 /**
@@ -49,7 +49,7 @@ CMH.options.settings = {
 CMH.options.defaultCheckServer = {
   url: 'https://checkmyhttps.net/',
   fingerprints: {
-    sha256: '8B944DBC442DA0892C307DEF6C971B671267BF033E8479C567E173973F970B9F'
+    sha256: '6D9BBB554CE7CD4420C26F60DD0831D40C34BB07F93E874CF631C24AB9F08F57'
   }
 }
 
