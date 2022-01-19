8c09b65cc227614720dc90238791a50b230b489c
Mathis Cadio <cadio@et.esiea.fr>
2022-01-19 13:20:06 +0100

    add import java.net.InetAddress to CMHPlugin.java

--- Mobile_App/CMHPlugin/src/android/CMHPlugin.java
+++ Mobile_App/CMHPlugin/src/android/CMHPlugin.java
@@ -12,6 +12,7 @@ import javax.net.ssl.HttpsURLConnection;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.net.InetAddress;
 import java.net.URL;
 import java.security.MessageDigest;
 import java.security.cert.Certificate;
